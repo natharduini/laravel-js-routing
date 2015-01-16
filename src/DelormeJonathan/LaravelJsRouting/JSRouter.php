eb2b4ff9c44d462ad24e76f8c63003b716372447
Nathalie Arduini <nathalie.arduini@gmail.com>
2015-01-16 02:07:25 +0100

    Use Config::get('app.url') instead of url() function to build the URLs (otherwise, doesn't work with sites in subfolders)

--- src/DelormeJonathan/LaravelJsRouting/JSRouter.php
+++ src/DelormeJonathan/LaravelJsRouting/JSRouter.php
@@ -1,27 +1,25 @@
 <?php namespace DelormeJonathan\LaravelJsRouting;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Config;
 
-class JSRouter
-{
-	public static function generate(){
+class JSRouter {
+	public static function generate() {
 		$router = Route::getRoutes();
 		$routesByAction = array();
 		$routesByName = array();
 
 		foreach ($router as $route) {
 			if ($route->getActionName() != 'Closure') {
-				$routesByAction[$route->getActionName()]['route'] = str_replace('?}', '}', Config::get('app.url') . url($route->getPath());
+				$routesByAction[$route->getActionName()]['route'] = str_replace('?}', '}', Config::get('app.url') . $route->getPath());
 				$routesByAction[$route->getActionName()]['parameters'] = $route->parameterNames();
 			}
 			if ($route->getName() != '') {
-				$routesByName[$route->getName()]['route'] = str_replace('?}', '}', Config::get('app.url') . url($route->getPath());
+				$routesByName[$route->getName()]['route'] = str_replace('?}', '}', Config::get('app.url') . $route->getPath());
 				$routesByName[$route->getName()]['parameters'] = $route->parameterNames();
 			}
 		}
-
 
 		return View::make('LaravelJsRouting::script', array('routesByAction' => json_encode($routesByAction), 'routesByName' => json_encode($routesByName)));
 	}
