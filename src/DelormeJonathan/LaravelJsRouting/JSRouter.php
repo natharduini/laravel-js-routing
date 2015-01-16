e45531bdc30a057d62b23a10c0b1799fe68bbd7b
Nathalie Arduini <nathalie.arduini@gmail.com>
2015-01-16 01:52:21 +0100

    Use Config::get('app.url') instead of url() function to build the URLs (otherwise, doesn't work with sites in subfolders)

--- src/DelormeJonathan/LaravelJsRouting/JSRouter.php
+++ src/DelormeJonathan/LaravelJsRouting/JSRouter.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Config;
 
 class JSRouter
 {
@@ -12,11 +13,11 @@ class JSRouter
 
 		foreach ($router as $route) {
 			if ($route->getActionName() != 'Closure') {
-				$routesByAction[$route->getActionName()]['route'] = str_replace('?}', '}', url($route->getPath()));
+				$routesByAction[$route->getActionName()]['route'] = str_replace('?}', '}', Config::get('app.url') . url($route->getPath());
 				$routesByAction[$route->getActionName()]['parameters'] = $route->parameterNames();
 			}
 			if ($route->getName() != '') {
-				$routesByName[$route->getName()]['route'] = str_replace('?}', '}', url($route->getPath()));
+				$routesByName[$route->getName()]['route'] = str_replace('?}', '}', Config::get('app.url') . url($route->getPath());
 				$routesByName[$route->getName()]['parameters'] = $route->parameterNames();
 			}
 		}
